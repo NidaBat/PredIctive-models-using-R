bc7f230bbf03a678baea5ebb668ed7b37c52587a
NidaBat <sana.sbatt@gmail.com>
2018-02-12 22:38:27 -0600

    My first commit

--- Swiftkey_Keyboard_Prediction_NPL/shiny_con.R
+++
@@ -1,4 +0,0 @@
-install.packages('rsconnect')
-library(rsconnect)
-#rsconnect::configureApp("Swiftkeyapp", size="large")
-rsconnect::setAccountInfo(name='nidabatt', token='202B571503D1AEFA473BC6A15820AEB8', secret='IJ+0MQk6wq+yuLZ5HMpqXCKrPTwUlHIMgtSybr0M')
